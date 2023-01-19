f177696974cd46a984f565b1a0489da1df4e1321
shvrekhan <shvrekhan@gmail.com>
2023-01-19 15:45:27 +0530

    header.js added

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>Snap Shot</h1>
+            </>
+        )
+    }
+}
+
+export default Header;
