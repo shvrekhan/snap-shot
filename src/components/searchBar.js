a7f8f26e5e758528d5c2f48744c4f302b8985efd
shvrekhan <shvrekhan@gmail.com>
2023-01-19 16:11:12 +0530

    search bar added

---
+++ src/components/searchBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <>
+                <div>
+                    <from>
+                        <input></input>
+                        <button></button>
+                    </from>
+                </div>
+            </>
+        )
+    }
+}
+
+export default SearchBar;
