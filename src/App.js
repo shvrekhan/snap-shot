badb1a95e362802ea363835c7388933d0803d243
shvrekhan <shvrekhan@gmail.com>
2023-01-19 22:04:06 +0530

    search component added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
-import Header from "./components/header";
 import SearchBar from "./components/searchBar"
+
 
 
 class App extends React.Component {
   render() {
     return (
-      < div >
-        <Header />
+      <>
         <SearchBar />
-      </div >
-
+      </>
     )
   }
 }
