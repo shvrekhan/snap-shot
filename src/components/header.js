f9c31a594978aef3ebce87d69bf79e4a141767fb
shvrekhan <shvrekhan@gmail.com>
2023-01-20 12:10:23 +0530

    header component added

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ class Header extends React.Component {
     render() {
         return (
             <>
-                <h1>Snap Shot</h1>
+                <h1 className="heading">Snap Shot</h1>
             </>
         )
     }
